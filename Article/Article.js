d358c5b5ba32290ac424d07c2ab2843a5a65c984
Adrian-Garcia <agarc5@hotmail.com>
2019-08-07 17:42:36 -0500

    added step 3

--- Article/Article.js
+++ Article/Article.js
@@ -152,14 +152,14 @@ window.addEventListener('load', (e)=> {
     return article;
   }
     // Step 2: Add an event listener to the expandButton span. This event listener should toggle the class 'article-open' on the 'article' div.
-     article.addEventListener('click', event => {
-        console.log('button clicked', event.target);
-        buttonOpen.classList.toggle('hide-btn');
-        buttonClose.classList.toggle('hide-btn');
-      });
+    //  article.addEventListener('click', event => {
+    //     console.log('button clicked', event.target);
+    //     buttonOpen.classList.toggle('hide-btn');
+    //     buttonClose.classList.toggle('hide-btn');
+    //   });
 
     // Step 3: return the entire component.
-       
+       console.log(createArticle);
     // Step 4: Map over the data, creating a component for each oject and add each component to the DOM as children of the 'articles' div.
       
     // Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new artible
