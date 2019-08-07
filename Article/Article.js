b8864efe678a41515359dabaae9ed51da0a552ea
Adrian-Garcia <agarc5@hotmail.com>
2019-08-07 16:09:28 -0500

    added articles to article.js

--- Article/Article.js
+++ Article/Article.js
@@ -112,3 +112,13 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new artible
 
 */
+ 
+createArticle = article =>{
+  const div = document.createElement('div');
+  const header = document.createElement('h2');
+  const date = document.createElement('p');
+  const p1 = document.createElement('p');
+  const p2 = document.createElement('p');
+  const p3 = document.createElement('p');
+  const span = document.createElement('span');
+}
